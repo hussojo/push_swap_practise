282b7fbb2b4814ceba0c1eecf4039d5ba51b7659
Josefina Husso <jhusso@c3r1p6.hive.fi>
2023-01-23 16:31:29 +0200

    simple array from argument

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jhusso <jhusso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 15:41:01 by jhusso            #+#    #+#             */
-/*   Updated: 2023/01/21 17:30:53 by jhusso           ###   ########.fr       */
+/*   Updated: 2023/01/23 16:21:08 by jhusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,66 @@
 #include <stdlib.h> // NULL, exit failure & success
 #include <stdio.h>
 
-int push(int *array, int *top, int n)
-{
-int i = 0;
-if(!n)
-	write(1, "nothing to add", 15);
+// works with "1 2 3 4" type of argument
 
-else
-	while(i++ < 3)
+int	ft_atoi(const char *nptr)
+{
+	int		sign;
+	long	res;
+
+	sign = 1;
+	res = 0;
+	while (*nptr == 32 || (*nptr >= 9 && *nptr <= 13))
+		nptr++;
+	if (*nptr == '-')
+		sign *= -1;
+	if (*nptr == '+' || *nptr == '-')
+		nptr++;
+	while (*nptr >= '0' && *nptr <= '9')
 	{
-		array[i+1] = array[i]; //here somethhhing shittttt
+		res = res * 10 + *nptr - '0';
+		nptr++;
+		if (res > 2147483647 && sign == 1)
+			return (-1);
+		else if (res < -2147483648 && sign != 1)
+			return (0);
 	}
-	array[0] = *top;
-	*top = n;
-return(1);
+	return ((int)res * sign);
 }
 
-int main(void)
+int array_len(char *argv)
 {
-	int array[3];
-	int *top = &array[0];
-	int i = 0;
+	int i;
 
-	push(array, top, 5);
-	push(array, top, 6);
-	push(array, top, 7);
-	printf("added: %d\n", array[0]); // "added: 7" correct!
-	printf("added: %d\n", array[1]); // "added: -357210144"
-	printf("added: %d\n", array[2]); // "added: -357210144"
-
-	// while(array[i]++ < 10) //endless loop
-	// 	printf("stack %d\n", array[i]);
-
-return(0);
+	i = 0;
+	while(argv[i])
+	{
+		i++;
+	}
+	return(i);;
 }
 
+void set_array(char *argv)
+{
+	int len;
+	int *stack_a;
+	int i;
+
+	len = array_len(argv);
+	stack_a = (int *)calloc(len + 1, sizeof (int ));
+	i = 0;
+	while (i < len)
+	{
+		printf("%d\n", ft_atoi(&argv[i]));
+		stack_a[i] =  ft_atoi(&argv[i]);
+		printf("stack_a[%d]= %d \nargv[%d]= %d\n", i, stack_a[i], i, argv[i]);
+		i++;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	if(argc == 2)
+		set_array(argv[1]);
+	return(0);
+}
