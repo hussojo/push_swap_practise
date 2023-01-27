c66b2395ad270585344f1d129dbd0e9837f2f7eb
Josefina Husso <jhusso@c3r5p6.hive.fi>
2023-01-27 10:19:44 +0200

    rights

--- Makefile
+++ Makefile
@@ -6,7 +6,40 @@
 #    By: jhusso <jhusso@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/01/21 15:49:29 by jhusso            #+#    #+#              #
-#    Updated: 2023/01/21 15:49:30 by jhusso           ###   ########.fr        #
+#    Updated: 2023/01/27 10:12:38 by jhusso           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+NAME = lps.a
+CC = cc
+RM = rm -f
+BUILD_FLAGS = -Wall -Wextra -Werror
+LIBFT = ./libft
+INCLUDE = ./libft/libft.h
+
+SRC =	main.c
+
+OBJ = $(SRC:.c=.o)
+
+H_FILES = push_swap.h
+
+all: $(NAME)
+
+$(NAME): $(SRC)
+	make -C ./libft/ all
+	mv ./libft/libft.a ./$(NAME)
+	$(CC) -c $(BUILD_FLAGS) $(SRC) -I $(INCLUDE)
+	ar rus $(NAME) $(OBJ)
+
+clean:
+	$(RM) $(OBJ)
+	cd $(LIBFT) && $(MAKE) clean
+
+fclean: clean
+	$(RM) $(NAME)
+	cd $(LIBFT) && $(MAKE) fclean
+
+re: fclean all
+
+
+.PHONY: all clean fclean re $(LIBFT)
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jhusso <jhusso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 15:41:01 by jhusso            #+#    #+#             */
-/*   Updated: 2023/01/27 09:38:33 by jhusso           ###   ########.fr       */
+/*   Updated: 2023/01/27 10:14:37 by jhusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,28 @@ int array_len(char *argv)
 
 void set_array(char *argv) // with spaces
 {
-	
+int len;
+	int *stack_a;
+	int i;
+	int j;
+
+	len = array_len(argv);
+	stack_a = (int *)calloc(len + 1, sizeof(int ));
+	i = 0;
+	while (i < len)
+	{
+		j = 0;
+		if(argv[i] == 9 || argv[i] == 32)
+			i++;
+		//printf("%d\n", ft_atoi(&argv[i]));
+		else
+		{
+			stack_a[j] = ft_atoi(&argv[i]);
+			i++;
+			j++;
+		}
+		printf("stack_a[%d]= %d \nargv[%d]= %d\n", i, stack_a[i], i, ft_atoi(&argv[i]));
+	}
 }
 
 int main(int argc, char *argv[])
