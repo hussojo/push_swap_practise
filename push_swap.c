b84bf931f604b62aebe68a543699776cee8b9076
Josefina Husso <jhusso@c3r5p6.hive.fi>
2023-01-29 18:48:44 +0200

    one/many arguments to stack works, is best function architecture?

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: jhusso <jhusso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 15:49:16 by jhusso            #+#    #+#             */
-/*   Updated: 2023/01/29 17:00:35 by jhusso           ###   ########.fr       */
+/*   Updated: 2023/01/29 18:47:39 by jhusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,46 +20,49 @@ char **ft_one_arg(char *av, char **st_a)
 	return(st_a);
 }
 
-char **ft_many_args(char *av, int ac, char **st_a)
-{
-	static int aux = 0;
-	st_a = (char **)malloc(sizeof(char *) * ac);
-	if(!st_a)
-		return NULL;
-	st_a[ac] = 0;
-	st_a[aux] = ft_strdup(av);
-	printf("st_a[%d] = %s\n", aux, st_a[aux]);
-	aux = aux +1;
-	return(st_a);
-}
+// char **ft_many_args(char *av, int ac, char **st_a)
+// {
+// 	static int aux = 0;
+// 	st_a = (char **)malloc(sizeof(char *) * ac);
+// 	if(!st_a)
+// 		return NULL;
+// 	st_a[ac] = 0;
+// 	st_a[aux] = ft_strdup(av);
+// 	printf("st_a[0] = %s\n", st_a[0]);
+// 	printf("st_a[1] = %s\n", st_a[1]);
+// 	aux = aux +1;
+// 	return(st_a);
+// }
 
 int main(int ac, char **av)
 {
 	static char **st_a;
 	// if(ac < 2)
 		//write(1, "error", 6);
+	st_a  = 0;
 	if(ac == 2)
 	{
 		st_a = ft_one_arg(av[1], st_a);
 	}
-
 	if(ac > 2)
 	{
 		int i;
 		int j;
 
-		i = 0;
-		while(++i < ac)
+		st_a = (char **)malloc(sizeof(char *) * ac);
+		if(!st_a)
+			printf("no stack allocated");
+
+		i = 1;
+		j = 0;
+		while(i < ac)
 		{
-			st_a = ft_many_args(av[i], ac, st_a);
-		}
-		printf("here\n");
-		j=0;
-		while(j < ac)
-		{
+			st_a[j] = ft_strdup(av[i]);
 			printf("st_a in main[%d] = %s\n", j, st_a[j]);
+			i++;
 			j++;
 		}
+		st_a[ac] = 0;
 	}
 	return(0);
 }
